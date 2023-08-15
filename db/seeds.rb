712dc0213fd6d929bd5fee1b70a1d38e9536c79a
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-15 20:25:16 +0300

    add logs for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
+puts 'Started seeding games'
 Game.states.each_value do |state|
   10.times { Game.create(state:) }
 end
 
+puts 'Started seeding players'
 30.times { Player.create(name: Faker::Name.name) }
+
+puts 'Done!'
+
