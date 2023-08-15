0e25c49f63ba14d1f09a3a9741f1f4ef2ba1acc5
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-15 20:13:47 +0300

    remove index on games, id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_132703) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_171025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_132703) do
     t.string "state", limit: 30, default: "created", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["id"], name: "index_games_on_id", unique: true
     t.index ["state"], name: "index_games_on_state"
   end
 
