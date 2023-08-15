5067e17bf7226cc2ba2f4840da74a0702b138826
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-15 16:59:21 +0300

    add players in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 Game.states.each do |state|
-  10.times { Game.create(state: state) }
+  10.times do
+    Game.create(state: state)
+  end
+end
+
+30.times do
+  Player.create(name: Faker::Name.name)
 end
