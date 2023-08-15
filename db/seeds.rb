a6352e32a887d4364b8261c0862982b703fa05a4
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-15 16:13:25 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,3 @@
-10.times do
-  Game.create(state: 'created')
-end
-
-10.times do
-  Game.create(state: 'in_progress')
-end
-
-10.times do
-  Game.create(state: 'completed')
-end
-
-10.times do
-  Game.create(state: 'canceled')
+Game.states.each do |state|
+  10.times { Game.create(state: state) }
 end
