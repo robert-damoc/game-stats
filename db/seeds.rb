c11edccf7ef8fbeecedfb2db953abf60291ad262
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-15 20:30:04 +0300

    use rails logger info instead of puts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
-puts 'Started seeding games'
+Rails.logger.info 'Started seeding games'
 Game.states.each_value do |state|
   10.times { Game.create(state:) }
 end
 
-puts 'Started seeding players'
+Rails.logger.info 'Started seeding players'
 30.times { Player.create(name: Faker::Name.name) }
 
-puts 'Done!'
-
+Rails.logger.info 'Done!'
