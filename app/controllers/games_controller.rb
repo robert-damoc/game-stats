77f616f8b982444bb393955f9ffa909b616465be
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-18 13:34:48 +0300

    sort games table in UI

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
 
   # GET /games or /games.json
   def index
-    @games = Game.all
+    @games = Game.order(params[:sort] || :id)
   end
 
   # GET /games/1 or /games/1.json
