7c8450bf74703789c62b671ba8da2bd4c40e704f
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-22 15:54:24 +0300

    sort and paginate players page in UI

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,16 @@
 class Player < ApplicationRecord
+  include Sortable
   validates :name, presence: true, length: { maximum: 30 }
+
+  def self.allowed_sort_columns
+    %w[name]
+  end
+
+  def self.default_sort_column
+    'name'
+  end
+
+  def self.default_direction
+    'asc'
+  end
 end
