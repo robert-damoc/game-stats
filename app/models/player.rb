f7c5f710059a4d2edc696cb74f78a38f2dad15a1
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-22 16:48:33 +0300

    after comments on pull request

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,6 @@
 class Player < ApplicationRecord
   include Sortable
+
   validates :name, presence: true, length: { maximum: 30 }
 
   def self.allowed_sort_columns
@@ -8,9 +9,5 @@ class Player < ApplicationRecord
 
   def self.default_sort_column
     'name'
-  end
-
-  def self.default_direction
-    'asc'
   end
 end
