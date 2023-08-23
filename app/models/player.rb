aa575bddac3ccf4d3af65d1ecbc95e69cafdc304
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-23 16:21:26 +0300

    add validation request in issue

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,6 +4,7 @@ class Player < ApplicationRecord
   has_many :games, through: :game_players
 
   validates :name, presence: true, length: { maximum: 30 }
+  validate :can_be_part_of_only_one_active_game, on: :create
 
   def self.allowed_sort_columns
     %w[name]
@@ -11,5 +12,13 @@ class Player < ApplicationRecord
 
   def self.default_sort_column
     'name'
+  end
+
+  private
+
+  def can_be_part_of_only_one_active_game
+    if games.in_progress.exists?
+      errors.add(:base, "A player can only be part of one active game at a time")
+    end
   end
 end
