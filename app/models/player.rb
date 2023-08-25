f4c199e47893c0cdef73a3b981fdd11578b0216c
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-25 11:53:48 +0300

    add `inverse of` on Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ApplicationRecord
   include Sortable
 
-  has_many :game_players, -> { order(position: :asc) }, dependent: nil
+  has_many :game_players, -> { order(position: :asc) }, dependent: nil, inverse_of: :game_players
   has_many :games, through: :game_players
 
   validates :name, presence: true, length: { maximum: 30 }
