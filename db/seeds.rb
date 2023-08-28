7651cea0796880c97e80e12dcac2eee9aeb8c68a
MariusTulbure <marius.tulbure00@gmail.com>
2023-08-28 11:54:44 +0300

    add seeds for gameplayer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-Rails.logger.info 'Started seeding games'
-Game.states.each_value do |state|
-  10.times { Game.create(state:) }
-end
+# Rails.logger.info 'Started seeding games'
+# Game.states.each_value do |state|
+#   10.times { Game.create(state:) }
+# end
 
-Rails.logger.info 'Started seeding players'
-30.times { Player.create(name: Faker::Name.name) }
+# Rails.logger.info 'Started seeding players'
+# 30.times { Player.create(name: Faker::Name.name) }
+
+Rails.logger.info 'Started seeding gameplayers'
+Game.all.each do |game|
+  Player.all.sample(3).each do |player|
+    GamePlayer.create!(game_id: game.id, player_id: player.id)
+  end
+end
 
 Rails.logger.info 'Done!'
