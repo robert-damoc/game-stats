fb30ae4234ab3ad9d804d7395361e5d012c07df6
Robert Mihail Damoc <robert.damoc@yahoo.com>
2023-09-26 19:45:20 +0300

    Add stimulus controller, to remove flash messages after 2 seconds

---
+++ app/javascript/controllers/removals_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="removals"
+export default class extends Controller {
+  connect() {
+    setTimeout(() => this.element.remove(), 2000)
+  }
+}
