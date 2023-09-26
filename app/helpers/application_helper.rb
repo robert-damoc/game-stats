8dbbd7640172d8216d6d5cb80d2dc4148d61417f
Robert Mihail Damoc <robert.damoc@yahoo.com>
2023-09-26 21:29:41 +0300

    Add inline_error_for for form errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
   def render_turbo_stream_flash_messages
     turbo_stream.prepend 'flash', partial: 'layouts/flash'
   end
+
+  def inline_error_for(field, form_obj)
+    html = []
+    if form_obj.errors[field].any?
+      html << form_obj.errors[field].map do |msg|
+        tag.div(msg, class: 'text-danger m-0 p-0 text-sm-end mb-2')
+      end
+    end
+    html.join.html_safe # rubocop:disable Rails/OutputSafety
+  end
 end
