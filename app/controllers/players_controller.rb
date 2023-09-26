8363cc11b897c2b648132f06eff828398d679e5f
MariusTulbure <137196597+MariusTulbure@users.noreply.github.com>
2023-09-26 18:40:33 +0300

    remove api from players controller (#50)

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -25,27 +25,21 @@ class PlayersController < ApplicationController
   def create
     @player = Player.new(player_params)
 
-    respond_to do |format|
-      if @player.save
-        format.html { redirect_to player_url(@player), notice: 'Player was successfully created.' }
-        format.json { render :show, status: :created, location: @player }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
-      end
+    if @player.save
+      redirect_to player_url(@player), notice: 'Player was successfully created.'
+    else
+      flash.now[:notice] = @player.errors.map(&:message).join(' ')
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /players/1 or /players/1.json
   def update
-    respond_to do |format|
-      if @player.update(player_params)
-        format.html { redirect_to player_url(@player), notice: 'Player was successfully updated.' }
-        format.json { render :show, status: :ok, location: @player }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
-      end
+    if @player.update(player_params)
+      redirect_to player_url(@player), notice: 'Player was successfully updated.'
+    else
+      flash.now[:notice] = @player.errors.map(&:message).join(' ')
+      render :edit, status: :unprocessable_entity
     end
   end
 
