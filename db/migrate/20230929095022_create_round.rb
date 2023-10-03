f11e3c08213de0dd889ad1f1a5ec9e5e6d68daf7
Marius Tulbure <marius.tulbure00@gmail.com>
2023-10-04 02:10:38 +0300

    add validations on round model
    rollback and migrate db again for rounds table to implement foreign Keys correctly

--- db/migrate/20230929095022_create_round.rb
+++ db/migrate/20230929095022_create_round.rb
@@ -1,8 +1,8 @@
 class CreateRound < ActiveRecord::Migration[7.0]
   def change
     create_table :rounds do |t|
-      t.uuid :game, foreign_key: true
-      t.uuid :player, foreign_key: true
+      t.references :game, foreign_key: true, type: :uuid
+      t.references :player, foreign_key: true, type: :uuid
       t.string :round_type, null: false, limit: 20
       t.integer :position
       t.jsonb :scores
