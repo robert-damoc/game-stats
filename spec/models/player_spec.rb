0f57cdca9ce8f61c415bc592c3026936d8342ade
Robert Mihail Damoc <robert.damoc@yahoo.com>
2023-10-18 20:35:10 +0300

    Initial spec for player model

---
+++ spec/models/player_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe Player do
+  describe '.allowed_sort_columns' do
+    subject(:allowed_sort_columns) { described_class.allowed_sort_columns }
+
+    it 'returns only the name' do
+      expect(allowed_sort_columns).to match_array %w[name]
+    end
+  end
+
+  describe '.default_sort_column' do
+    subject(:default_sort_column) { described_class.default_sort_column }
+
+    it 'returns only the name' do
+      expect(default_sort_column).to eq 'name'
+    end
+  end
+
+  describe 'validations' do
+    context 'when validating the name presence' do
+      context 'with name provided' do
+        let(:player_name) { nil }
+
+        it 'raises ActiveRecord::RecordInvalid' do
+          expect { described_class.create!(name: player_name) }.to raise_error(ActiveRecord::RecordInvalid)
+        end
+
+        it 'does not create the record' do
+          expect { described_class.create(name: player_name) }.not_to change(described_class, :count)
+        end
+      end
+
+      context 'without name provided' do
+        let(:player_name) { Faker::Name.first_name }
+
+        it 'does not raise ActiveRecord::RecordInvalid' do
+          expect { described_class.create!(name: player_name) }.not_to raise_error
+        end
+
+        it 'creates the record' do
+          expect { described_class.create(name: player_name) }.to change(described_class, :count).by(1)
+        end
+      end
+    end
+  end
+end
