1c4f6d7bab93caa57d92c6a75baf3811a722f1cf
Marius Tulbure <marius.tulbure00@gmail.com>
2023-10-20 12:52:40 +0300

    Delete unnecessary validations

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,34 +1,21 @@
 class Round < ApplicationRecord
-
   validates :round_type, presence: true
-  validates :game_id, uniqueness: true, message: 'This game already started or has been played.'
   validates :player_id,
             uniqueness: true,
             scope: %i[game_id round_type],
             message: 'A player can only have one round of a given type per game.'
-  validate :unique_round_type_per_game
 
   belongs_to :game
   belongs_to :player
 
   enum round_type: {
-    king: 'king',
-    queens: 'queens',
-    diamonds: 'diamonds',
-    levate: 'levate',
-    rentz_minus: 'rentz_minus',
-    rentz_plus: 'rentz_plus',
-    whist: 'whist',
-    ten: 'ten',
-    totale_minus: 'totale_minus',
-    totale_plus: 'totale_plus'
+    rentz_minus: 'Rentz-',
+    rentz_plus: 'Rentz+',
+    totale_minus: 'Totale-',
+    totale_plus: 'Totale+',
+    king: 'King of hearts',
+    ten: 'Ten of clubs',
+    queens: 'Queens',
+    diamonds: 'Diamonds'
   }
-
-  private
-
-  def unique_round_type_per_game
-    if game.rounds.exists?(round_type: round_type, player_id: player_id)
-      errors.add(:round_type, "Already played #{round_type}. You can play every type of round only one time.")
-    end
-  end
 end
