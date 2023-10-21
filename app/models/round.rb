eaf5663d53e436814ea60742f6d6c168f7d9b1bc
Marius Tulbure <marius.tulbure00@gmail.com>
2023-10-21 20:00:41 +0300

    fix validation and add position

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,21 +1,28 @@
 class Round < ApplicationRecord
+  acts_as_list scope: :game
+
   validates :round_type, presence: true
-  validates :player_id,
-            uniqueness: true,
-            scope: %i[game_id round_type],
-            message: 'A player can only have one round of a given type per game.'
+
+  after_validation :ensure_uniqueness
 
   belongs_to :game
   belongs_to :player
 
   enum round_type: {
-    rentz_minus: 'Rentz-',
-    rentz_plus: 'Rentz+',
-    totale_minus: 'Totale-',
-    totale_plus: 'Totale+',
+    rentz_minus: 'Rentz -',
+    rentz_plus: 'Rentz +',
+    totale_minus: 'Totale -',
+    totale_plus: 'Totale +',
     king: 'King of hearts',
     ten: 'Ten of clubs',
     queens: 'Queens',
     diamonds: 'Diamonds'
   }
+
+  def ensure_uniqueness
+    return unless Round.where(player_id:, game_id:, round_type:).where.not(id:).any?
+
+    errors.add(:base, 'Round with the same (player_id, game_id, round_type) already exists!')
+    raise ActiveRecord::RecordInvalid, self
+  end
 end
