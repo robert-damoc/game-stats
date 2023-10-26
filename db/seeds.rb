6db323e05af48a07160c39b9fb4f24ba8bba5ca2
Marius Tulbure <marius.tulbure00@gmail.com>
2023-10-26 18:28:14 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-# puts 'Started seeding Games'
-# Game.states.each_value do |state|
-#   games_to_create = 10 - Game.where(state:).count
-#   games_to_create.times { Game.create(state:) } if games_to_create.positive?
-# end
+puts 'Started seeding Games'
+Game.states.each_value do |state|
+  games_to_create = 10 - Game.where(state:).count
+  games_to_create.times { Game.create(state:) } if games_to_create.positive?
+end
 
-# puts 'Started seeding Players'
-# (30 - Player.count).times { Player.create(name: Faker::Name.name) }
+puts 'Started seeding Players'
+(30 - Player.count).times { Player.create(name: Faker::Name.name) }
 
-# puts 'Started seeding GamePlayers'
-# Game.find_each do |game|
-#   players = Player.order('RANDOM()').limit(3)
+puts 'Started seeding GamePlayers'
+Game.find_each do |game|
+  players = Player.order('RANDOM()').limit(3)
 
-#   players.each do |player|
-#     game.game_players.create(player:)
-#   end
-# end
+  players.each do |player|
+    game.game_players.create(player:)
+  end
+end
 
 puts 'Started seeding Rounds'
 GamePlayer.find_each do |game_player|
