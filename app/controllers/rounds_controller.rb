7588d16141cdf53f06ec57ed13207590631d7a7e
Marius Tulbure <marius.tulbure00@gmail.com>
2023-10-30 23:01:59 +0200

    change to update method
    also some change to view/rounds/edit file

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,5 +1,5 @@
 class RoundsController < ApplicationController
-  before_action :set_game_and_players
+  before_action :set_game
   before_action :set_round, only: %i[edit update destroy]
 
   def new
@@ -21,8 +21,6 @@ class RoundsController < ApplicationController
 
   def update
     if @round.update(round_params)
-      # update_scores
-
       redirect_to game_path(@game), notice: 'Scores updated successfully.'
     else
       flash.now[:notice] = @round.errors.map(&:message).join(' ')
@@ -39,31 +37,14 @@ class RoundsController < ApplicationController
   private
 
   def round_params
-    params.require(:round).permit(:game_player_id, :round_type, :position, :scores)
+    params.require(:round).permit(:game_player_id, :round_type, scores: {})
   end
 
-  def set_game_and_players
-    @game = Game.find_by(id: params[:game_id])
-    @players = @game.player_ids
-    @game_players = @game.game_players
+  def set_game
+    @game = Game.find(params[:game_id])
   end
 
   def set_round
     @round = @game.rounds.find(params[:id])
-  end
-
-  def update_scores
-    @game.players.each do |player|
-      updated_scores = params[:scores][player_name]
-
-      current_scores = @round.scores[player.id.to_s] || {}
-
-      if updated_scores.is_a?(String)
-        current_scores[player.id] = updated_scores.to_i
-        @round.scores[player.id.to_s] = current_scores
-      end
-    end
-
-    @round.save!
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -3,7 +3,6 @@ class Player < ApplicationRecord
 
   has_many :game_players, -> { order(position: :asc) }, dependent: :destroy, inverse_of: :player
   has_many :games, through: :game_players
-  has_many :rounds, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 30 }
 
