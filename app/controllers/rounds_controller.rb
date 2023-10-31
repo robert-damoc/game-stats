f2d799ab5ad4143138271d3db56eeb4943fa12a2
Marius Tulbure <137196597+MariusTulbure@users.noreply.github.com>
2023-10-31 18:54:15 +0200

    44 create update round controller (#56)

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,43 +1,49 @@
 class RoundsController < ApplicationController
+  before_action :set_game
   before_action :set_round, only: %i[edit update destroy]
 
   def new
-    @round = Round.new
+    @round = @game.rounds.new
   end
 
   def edit; end
 
   def create
-    @game_player = GamePlayer.find(params[:game_player_id])
-    @round = @game_player.rounds.build(round_params)
+    @round = @game.rounds.new(round_params)
 
     if @round.save
-      redirect_to game_round_path(@game_player.game, @round), notice: 'Round was successfully created.'
+      redirect_to game_path(@game), notice: 'Round was successfully created.'
     else
+      flash.now[:notice] = @round.errors.map(&:message).join(' ')
       render :new, status: :unprocessable_entity
     end
   end
 
   def update
     if @round.update(round_params)
-      redirect_to round_url(@round), notice: 'Round was successfully updated.'
+      redirect_to game_path(@game), notice: 'Scores updated successfully.'
     else
+      flash.now[:notice] = @round.errors.map(&:message).join(' ')
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @round.destroy
-    redirect_to rounds_url, notice: 'Round was successfully destroyed.'
+    redirect_to game_path(@game), notice: 'Round was successfully destroyed.'
   end
 
   private
 
-  def set_round
-    @round = Round.find(params[:id])
+  def round_params
+    params.require(:round).permit(:game_player_id, :round_type, scores: {})
   end
 
-  def round_params
-    params.require(:round).permit(:game_player_id, :round_type, :position, :scores)
+  def set_game
+    @game = Game.find(params[:game_id])
+  end
+
+  def set_round
+    @round = @game.rounds.find(params[:id])
   end
 end
