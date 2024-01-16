3a513b8d528c98fa9085cc0245cff42b454f8d71
Marius Tulbure <marius.tulbure00@gmail.com>
2024-01-16 20:37:11 +0200

    Add rescue from RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found(exception)
+    raise exception if Rails.env.development?
+
+    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+  end
 end
