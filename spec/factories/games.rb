de5a9b6b6c729540b43688d7b06eb8674f974664
Marius Tulbure <marius.tulbure00@gmail.com>
2024-02-09 14:55:48 +0200

    add game update specs

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :game do
-    state { 'created' }
+    trait :created do
+      state { 'created' }
+    end
+
+    trait :in_progress do
+      state { 'in_progress' }
+    end
+
+    trait :completed do
+      state { 'completed' }
+    end
+
+    trait :canceled do
+      state { 'canceled' }
+    end
   end
 end
--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -238,4 +238,123 @@ describe 'Games' do
       end
     end
   end
+
+  describe 'PUT /games/:id' do
+    subject(:update_game) { put game_path(id), params: }
+
+    let(:assigned_game) { assigns(:game) }
+    let(:id) { create(:game).id }
+    let(:new_state) { 'created' } # Replace with the actual valid state
+    let(:params) { { game: { state: new_state, player_ids: updated_player_ids } } }
+    let(:updated_player_ids) { [first_player, second_player] }
+    let(:first_player) { create(:player).id }
+    let(:second_player) { create(:player).id }
+
+    before { update_game }
+
+    context 'when params is empty' do
+      it do
+        aggregate_failures do
+          expect(flash.now[:notice]).to eq 'Game was successfully updated.'
+          expect(assigned_game.errors.full_messages).to be_empty
+          expect(assigned_game.reload.state).to eq 'created'
+          expect(response).to have_http_status(:found)
+        end
+      end
+    end
+
+    context 'when current state is in_progress' do
+      let(:new_state) { 'in_progress' }
+
+      context 'when there are less than MIN_PLAYERS_PER_GAME' do
+        let(:updated_player_ids) { [] }
+
+        it do
+          aggregate_failures do
+            expect(flash.now[:notice]).to eq(
+              "Minimum number of players to start the game is #{Game::MIN_PLAYERS_PER_GAME}"
+            )
+            expect(assigned_game.errors[:game_players]).to include(
+              "Minimum number of players to start the game is #{Game::MIN_PLAYERS_PER_GAME}"
+            )
+            expect(assigned_game.reload.state).to eq 'created'
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+        end
+      end
+
+      context 'when the number of players is between MIN_PLAYERS_PER_GAME and MAX_PLAYERS_PER_GAME' do
+        let(:updated_player_ids) { Array.new(Game::MIN_PLAYERS_PER_GAME + 1) { create(:player).id } }
+
+        it do
+          aggregate_failures do
+            expect(response).to redirect_to(edit_game_url(assigned_game))
+            expect(flash.now[:notice]).to eq 'Game was successfully updated.'
+            expect(assigned_game.errors).to be_empty
+            expect(assigned_game.reload.state).to eq 'in_progress'
+            expect(assigned_game.players.pluck(:id)).to match_array(updated_player_ids)
+            expect(response).to have_http_status(:found)
+          end
+        end
+      end
+
+      context 'when there are more than MAX_PLAYERS_PER_GAME' do
+        let(:updated_player_ids) { Array.new(Game::MAX_PLAYERS_PER_GAME + 1) { create(:player).id } }
+
+        it do
+          aggregate_failures do
+            expect(flash.now[:notice]).to eq(
+              "There can be a maximum of #{Game::MAX_PLAYERS_PER_GAME} players in a game."
+            )
+            expect(assigned_game.errors[:game_players]).to include(
+              "There can be a maximum of #{Game::MAX_PLAYERS_PER_GAME} players in a game."
+            )
+            expect(assigned_game.reload.state).to eq 'created'
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+        end
+      end
+    end
+
+    context 'when state completed' do
+      let(:new_state) { 'completed' }
+
+      it do
+        aggregate_failures do
+          expect(flash.now[:notice]).to eq 'invalid state transition'
+          expect(assigned_game.errors[:state]).to include('invalid state transition')
+          expect(assigned_game.reload.state).not_to eq new_state
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
+
+    context 'when state is canceled' do
+      let(:new_state) { 'canceled' }
+
+      it do
+        aggregate_failures do
+          expect(response).to redirect_to(edit_game_url(assigned_game))
+          expect(flash.now[:notice]).to eq 'Game was successfully updated.'
+          expect(assigned_game.errors).to be_empty
+          expect(assigned_game.reload.state).to eq new_state
+          expect(response).to have_http_status(:found)
+        end
+      end
+    end
+
+    context 'when id is not UUID' do
+      let(:id) { '1' }
+
+      it { expect(response).to have_http_status(:not_found) }
+      it { expect(assigned_game).to be_nil }
+    end
+
+    context 'when game does not exist' do
+      let(:id) { SecureRandom.uuid }
+
+      it { expect(response).to have_http_status(:not_found) }
+      it { expect(assigned_game).to be_nil }
+    end
+  end
 end
